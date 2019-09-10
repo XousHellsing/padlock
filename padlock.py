bc0c22039c7c703bb588b00686933a98d9778a33
XousHellsing <48122849+XousHellsing@users.noreply.github.com>
2019-09-10 00:34:04 -0500

    Add files via upload

---
+++ padlock.py
@@ -0,0 +1,16 @@
+import os
+import locker as m
+import time
+
+x=0
+vec=[]
+def main():
+	print("Procesando")
+	for y in range(0,5):
+		print("read...",y)
+		time.sleep(1)
+def pad():
+	m.padlock(x)
+main()
+pad()
+os.system("pause")
