9377780ffa99c42739cc6d56b96e421e604461b7
XousHellsing <48122849+XousHellsing@users.noreply.github.com>
2019-09-10 00:19:49 -0500

    Create locker.py

---
+++ locker.py
@@ -0,0 +1,49 @@
+import os
+import time
+import numpy as np
+import random
+vec=[]
+lock=0
+for x in range(0,10):
+	vec.append(x+1) 
+print(vec)
+def sumaF(lock):
+	os.system("cls")
+	print("Suma de vector")
+	a=0
+	if lock==0:
+		time.sleep(1)
+		for x in range(0,10):
+			a=a+vec[x]
+			print(a)
+	else:
+		print("ocupado s")
+def multiF(lock):
+	os.system("cls")
+	print("Multiplicacion de vector")
+	b=1
+	if lock==0:
+		time.sleep(4)
+		b=np.prod(vec)
+		print(b)
+	else:
+		print("ocupado m")
+
+def padlock(lock):
+	lock1=lock
+	x=random.randrange(0,1000)
+	pad=""
+	print("Execute? y/n...")
+	pad=input()
+	while pad=="y":
+		if random.randrange(0,1000)<=500:
+			sumaF(lock)
+		if random.randrange(0,1000)>=500:
+			multiF(lock)
+		print("Want repeat? y/n...")
+		pad=input()
+	else:
+		print("bye")
+
+
+
